8eabc1bd1e7c8344a721ed89d8474c20a26451c2
Alexander Burke <ollie0burke@gmail.com>
2024-01-15 11:36:10 +0000

    now can be written to

--- cluster_sub/submit_job.sh
+++ cluster_sub/submit_job.sh
@@ -11,17 +11,12 @@
 #SBATCH --mem=100G
 #SBATCH --cpus-per-gpu=20
 #SBATCH --time=06:00:00
-#SBATCH --mail-user=<ollie.burke@l2it.in2p3.fr>
-#SBATCH --mail-type=ALL
 
-module load conda
-module unload conda
 module load conda
 conda activate vanilla_few
 module load gcc/10.2.0
 module load cuda/11.7.0
 
-python /home/ad/burkeol/work/EMRI_PE/mcmc_code/mcmc_run.py 
 
 
 
