cacf9c9e3698040bfc327cb4b53cc561df5b043b
OllieBurke <ollie0burke@gmail.com>
2024-01-15 12:23:02 +0000

    fixed bug!

--- setup_files/setup_file.sh
+++ setup_files/setup_file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo Now going to build environments and set up dependencies. 
-
+#echo Now going to build environments and set up dependencies. 
+#
 cd .. # enter root directory
 
 echo building directory for data files
@@ -11,8 +11,8 @@ echo Loading conda module
 module load conda  # CNES cluster, need to load conda prior to using it
 
 echo Now creating environment      # Set up conda environment -- vanilla_few
-conda create -n vanilla_few -c conda-forge gcc_linux-64 gxx_linux-64 wget gsl lapack=3.6.1 hdf5 numpy Cython scipy tqdm jupyter ipython h5py requests matplotlib corner python=3.9 
-conda activate vanilla_few         
+conda create -n test_EMRI_env -c conda-forge gcc_linux-64 gxx_linux-64 wget gsl lapack=3.6.1 hdf5 numpy Cython scipy tqdm jupyter ipython h5py requests matplotlib corner python=3.9 
+conda activate test_EMRI_env         
 
 echo Installing cupy-cuda11x toolkit
 pip install cupy-cuda11x           # Warning: this is SPECIFIC to the CNES cluster for the GPUs available
@@ -33,8 +33,8 @@ git clone https://github.com/BlackHolePerturbationToolkit/FastEMRIWaveforms.git
 echo Now installing LISAanalysistools 
 cd LISAanalysistools
 python setup.py install
-cd ../lisa-on-gpu
 
+cd ../lisa-on-gpu
 echo Now installing lisa-on-gpu 
 python setup.py install
 
@@ -42,7 +42,7 @@ echo Now installing FastEMRIWaveforms
 cd ../FastEMRIWaveforms
 python setup.py install
 
-cd .. # Get back to root directory
+cd ../../ # Get back to root directory
 
 # Now to set up submit file.
 
@@ -55,6 +55,8 @@ insert_text() {
     # Use awk to insert the text at the specified line in the target file
     awk -v line="$line_number" -v text="$text" 'NR==line {$0=text} {print}' "$target_file" > temp && mv temp "$target_file"
 }
+
+echo $pwd
 
 cd mcmc_code
 exec_path=$(pwd) # Extract path of python code to be executed
